bc78393030de50ba51cb09979f0efc3b6620b039
«Alexandr» <surgut1987@icloud.com>
2023-05-24 22:12:46 +0300

    дз из первого семинара

--- Lesson1/Program.cs
+++ Lesson1/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее
+
+// Console.WriteLine("введите два числа");
+// int a = int.Parse(Console.ReadLine()), b = int.Parse(Console.ReadLine());
+// if (a >= b){
+//     Console.WriteLine($"max = {a}");
+// }
+// else{
+//     Console.WriteLine($"max = {b}");
+// }
+
+
+// напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел
+
+// Console.WriteLine("введите три числа");
+// int a = int.Parse(Console.ReadLine()), b = int.Parse(Console.ReadLine()), c = int.Parse(Console.ReadLine());
+// int max = a;
+// if (b > a) max = b;
+// if (c > max) max = c;
+// Console.WriteLine(max);
+
+// напишите программу, котораяна вход принимает число и выдаёт, является ли число четным
+
+// Console.WriteLine("введите число");
+// int digit = int.Parse(Console.ReadLine());
+// if (digit % 2 == 0){
+//     Console.WriteLine("да");
+// }
+// else{
+//     Console.WriteLine("нет");
+// }
+
+// напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
+
+// Console.WriteLine("введите число N");
+// int N = int.Parse(Console.ReadLine());
+// for (int i = 1; i <= N; i++){
+//     if (i % 2 == 0){
+//         Console.Write(i);
+//     }
+//     Console.Write(' ');
+// }
